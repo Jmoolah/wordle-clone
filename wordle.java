34616e8e25e9f3021b42214f8aacb4f07e74b7b0
Jmoolah <tiraquelindo@gmail.com>
2025-12-12 19:52:26 -0500

    memorizing the git crap

--- wordle.java
+++ wordle.java
@@ -14,10 +14,10 @@ public class wordle {
     public static final String GREEN = "\u001B[32m";
     public static final String YELLOW = "\u001B[33m";
     
-    public static HashSet<Character> greychars = new HashSet<>();
-    public static HashSet<Character> yellowChars = new HashSet<>();
+    public static HashSet<String> greychars = new HashSet<>();
+    public static HashSet<String> yellowChars = new HashSet<>();
     public static HashSet<String> greenChars = new HashSet<>();
-    public static HashSet<Character> unsedChars = new HashSet<>();
+    public static HashSet<String> unsedLetters = new HashSet<>();
     public static ArrayList<String> letterColors = new ArrayList<>();
     
     public static HashMap<Character, Integer> correctLetterLocations = new HashMap<>();
@@ -29,9 +29,11 @@ public class wordle {
      public static byte guessCount=0;
    
     public static void main(String[] args) throws IOException {
+        System.out.print("\u001B[2J"); // ANSI code: clear entire screen
+
         for (char c = 'A'; c <= 'Z'; c++) {
         //System.out.print(c + " ");
-        unsedChars.add(c);
+        unsedLetters.add(String.valueOf(c));
         letterColors.add(Character.toString(c));
         
         }
@@ -39,13 +41,21 @@ public class wordle {
        //System.out.print(String.join(" ", letterColors));
         makeWordist(FILENAME);
         getACorrectWord();
-       // System.out.println(correctWord);
+        System.out.println(correctWord);
         
-       
+       int update=0;
         while (isGuesswrong && guessCount!=5) {
+           printLetterColors(update);
            takeGuess();
            guessCount++;
-           System.out.println(greenChars);
+          /* System.out.print(RESET+ GREEN + greenChars + RESET);
+           System.out.print(RESET+ YELLOW+  yellowChars + RESET);
+           System.out.print(RESET+  unsedLetters + RESET);*/
+           System.out.println();
+           update++;
+        }
+        if(isGuesswrong){
+            System.out.println("Correct Word: "+ correctWord);
         }
         System.out.println(RESET);
     }
@@ -97,7 +107,7 @@ public class wordle {
 
     */
     public static void takeGuess() {
-        System.out.println();
+        
         boolean isGuessValid = false;
         String guessWhile ="";
         while(!isGuessValid){
@@ -116,7 +126,6 @@ public class wordle {
 
         String guess = guessWhile.toUpperCase();
         char[] correctWordCharArr = correctWord.toString().toCharArray();
-        
         char[] guessCharArr = guess.toCharArray();
 
         System.out.print("\033[F");   // move cursor UP one line
@@ -133,22 +142,35 @@ public class wordle {
             if(guessCharArr[c]==correctWordCharArr[c]){
                 greenChars.add(String.valueOf(guessCharArr[c]));
                 System.out.print(GREEN + guessCharArr[c]+RESET);
+                //unsedLettrs.remove(String.valueOf(guessCharArr[c]));
+               if( yellowChars.contains(String.valueOf(guessCharArr[c]))){
+                    yellowChars.remove(String.valueOf(guessCharArr[c]));
+               }
+                
+                
                 
             }
             else if(correctWordCharcters.contains(guessCharArr[c])){
                System.out.print(YELLOW +guessCharArr[c]+RESET); 
-               yellowChars.add((char) c);
+               yellowChars.add(String.valueOf(guessCharArr[c]));
+               unsedLetters.remove(String.valueOf(guessCharArr[c]));
             }
             else{
                 System.out.print(guessCharArr[c]);
-                greychars.add((char) c);
+                greychars.add(String.valueOf(guessCharArr[c]));
+                
             }
         }
+
        
 
     }
 
-    public void updateColors(int updateRange){
-        
-    }
+    public static void printLetterColors(int updateRange){
+         System.out.print("\u001B[H"); // ANSI code: move cursor to top-left corner
+         System.out.println("Unsed Letter:\n"+unsedLetters);
+         System.out.print("Wrong Letters:\n"+ greychars);
+         System.out.print("\033[" + updateRange +"B");  // moves cursor down 5 lines
+
+    }  
 }
